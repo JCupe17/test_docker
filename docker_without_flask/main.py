37b619afb2e47ce8554611dcfe2d8629d1fb628c
jcupe <jeancupe1@gmail.com>
2020-07-08 10:22:53 +0200

    Test - First part without flask. Problems from 1 to 3.

---
+++ docker_without_flask/main.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import os
+import sys
+from sparseArray import SparseArray
+
+
+if __name__ == '__main__':
+
+    # Verify the number of arguments to run the script
+    if len(sys.argv) > 2:
+        print("[ERROR] Expected 2 arguments but got {} arguments.".format(len(sys.argv)))
+    else:
+        # Prepare the sparseArray instance
+        stringData = open(os.environ['INPUT_PATH'], mode='r').read().splitlines()
+        sparseArray = SparseArray(stringData)
+        print(sparseArray)
+
+        # Prepare the query
+        query = sys.argv[1]
+        query = query.split(',')
+
+        # Prepare the output result
+        queryResult = sparseArray.matching_strings(query)
+        queryDict = {k: v for k, v in zip(query, queryResult)}
+
+        print("Query Size: {}".format(sparseArray.querySize))
+        print("{}".format(queryDict))
